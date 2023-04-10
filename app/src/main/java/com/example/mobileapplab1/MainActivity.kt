16fc4536200d68d0ad07f518c3da2b625c5c13df
Artyom Bogaichuk <bogaichukartem@gmail.com>
2023-04-10 22:23:49 +0700

    splash screen

--- app/src/main/java/com/example/mobileapplab1/MainActivity.kt
+++ app/src/main/java/com/example/mobileapplab1/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.mobileapplab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.os.Handler
+import android.content.Intent
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, OnboardingActivity::class.java)
+            startActivity(intent)
+        }, 3000)
     }
 }
